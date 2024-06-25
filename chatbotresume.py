ba055c5a26157e20711e6c896eda09b422e2b48c
LAVANYA <22051433@kiit.ac.in>
2024-06-25 12:43:52 +0530

    Add new files

---
+++ chatbotresume.py
@@ -0,0 +1,89 @@
+import streamlit as st
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.llms import Replicate
+from langchain_community.vectorstores import FAISS
+from langchain_community.document_loaders import Docx2txtLoader
+
+from streamlit_chat import message
+from langchain.chains import ConversationalRetrievalChain
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.memory import ConversationBufferMemory
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def initialize_session_state():
+    if 'history' not in st.session_state:
+        st.session_state['history'] = []
+
+    if 'generated' not in st.session_state:
+        st.session_state['generated'] = ["Hello! You can ask me anything related to Lavanya"]
+
+    if 'past' not in st.session_state:
+        st.session_state['past'] = ["Hey"]
+
+def conversation_chat(query, chain, history):
+    result = chain({"question": query, "chat_history": history})
+    history.append((query, result["answer"]))
+    return result["answer"]
+
+def display_chat_history(chain):
+    reply_container = st.container()
+    container = st.container()
+
+    with container:
+        with st.form(key='my_form', clear_on_submit=True):
+            user_input = st.text_input("Question:", placeholder="Ask about the document", key='input')
+            submit_button = st.form_submit_button(label='Send')
+
+        if submit_button and user_input:
+            with st.spinner('Generating response...'):
+                output = conversation_chat(user_input, chain, st.session_state['history'])
+
+            st.session_state['past'].append(user_input)
+            st.session_state['generated'].append(output)
+
+    if st.session_state['generated']:
+        with reply_container:
+            for i in range(len(st.session_state['generated'])):
+                message(st.session_state["past"][i], is_user=True, key=str(i) + '_user', avatar_style="thumbs")
+                message(st.session_state["generated"][i], key=str(i), avatar_style="fun-emoji")
+
+def create_conversational_chain(vector_store):
+    load_dotenv()
+    llm = Replicate(
+        streaming=True,
+        model="replicate/llama-2-70b-chat:58d078176e02c219e11eb4da5a02a7830a283b14cf8f94537af893ccff5ee781",
+        callbacks=[StreamingStdOutCallbackHandler()],
+        model_kwargs={"temperature": 0.01, "max_length": 500, "top_p": 1}
+    )
+    memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+    chain = ConversationalRetrievalChain.from_llm(llm=llm, retriever=vector_store.as_retriever(search_kwargs={"k": 2}), memory=memory)
+    return chain
+
+def main():
+    load_dotenv()
+    initialize_session_state()
+    st.title("Chatbot")
+    file_path = "C:\\Users\\KIIT\\Desktop\\web scrapping\\scrapp.docx"  # Updated file path to your .docx document
+
+    if os.path.exists(file_path):
+        loader = Docx2txtLoader(file_path)
+        documents = loader.load()
+
+        text_splitter = CharacterTextSplitter(separator="\n", chunk_size=1000, chunk_overlap=100, length_function=len)
+        text_chunks = text_splitter.split_documents(documents)
+
+        embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2", model_kwargs={'device': 'cpu'})
+
+        vector_store = FAISS.from_documents(text_chunks, embedding=embeddings)
+
+        chain = create_conversational_chain(vector_store)
+
+        display_chat_history(chain)
+
+if __name__ == "__main__":
+    main()
